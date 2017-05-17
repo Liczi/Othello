0ce7a3570cc05f000766f2e71985961704713367
Liczi <jak.liczner@gmail.com>
2017-05-17 16:40:59 +0200

    testing RESTful service

--- web/src/main/resources/components/src/app/app.component.ts
+++ web/src/main/resources/components/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ApiService} from './api/api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
+
+    constructor(private apiService: ApiService) {
+    }
+
+    ngOnInit() {
+        this.loadTitle();
+    }
+
+    loadTitle() {
+        this.apiService.getTitle()
+            .subscribe(title => this.title = title, err => {
+                console.log(err)
+            });
+    }
 }
---
+++ web/src/main/resources/components/src/test/test.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Jakub on 19.05.2017.
+ */
+export class Test {
+    constructor(public integer: number,
+                public astring: string,
+                public aBollean: boolean,
+                public tableBool: boolean[]) {
+    }
+}
